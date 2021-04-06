c8572df0fddafb22296968e343e3efbd1f8d3835
ryananshar <ansharryan@gmail.com>
2021-04-06 10:38:52 +0700

    feat: init spring boot project

---
+++ pebelco/src/main/java/propensi/tugas/pebelco/PebelcoApplication.java
@@ -0,0 +1,13 @@
+package propensi.tugas.pebelco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PebelcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PebelcoApplication.class, args);
+	}
+
+}
---
+++ pebelco/src/test/java/propensi/tugas/pebelco/PebelcoApplicationTests.java
@@ -0,0 +1,13 @@
+package propensi.tugas.pebelco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PebelcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
