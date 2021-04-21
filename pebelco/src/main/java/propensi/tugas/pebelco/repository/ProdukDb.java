9248a5edb1aded2d3e3a499a9f4e27b9461e251b
ryananshar <ansharryan@gmail.com>
2021-04-21 22:35:18 +0700

    change produk db

--- pebelco/src/main/java/propensi/tugas/pebelco/repository/ProdukDb.java
+++ pebelco/src/main/java/propensi/tugas/pebelco/repository/ProdukDb.java
@@ -1,7 +1,5 @@
 package propensi.tugas.pebelco.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
