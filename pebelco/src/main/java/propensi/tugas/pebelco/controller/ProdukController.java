57dbdfcb04089aa930749a7f030ca14459a60a77
Marcel Valdhano <marcelvaldhano@gmail.com>
2021-05-17 13:26:44 +0700

    Fixing Iteration Produk and Tag Produk

--- pebelco/src/main/java/propensi/tugas/pebelco/controller/ProdukController.java
+++ pebelco/src/main/java/propensi/tugas/pebelco/controller/ProdukController.java
@@ -81,48 +81,13 @@ public class ProdukController {
     public String konfirmasiHapusProduk(
             @PathVariable Long id,
             Model model) {
-
         model.addAttribute("pop", "konfirmasi hapus");
         model.addAttribute("msg2", "Konfirmasi Penghapusan");
         model.addAttribute("subMsg", "Apakah anda yakin ingin menghapus Produk ini?");
         model.addAttribute("produk", produkService.findAll());
         model.addAttribute("id", id);
-
-
         return "produk/daftar-produk";
     }
-
-    @RequestMapping(value="/produk/tambah", params={"removeRow"})
-    public String removeRow(
-            @ModelAttribute ProdukModel produk, Model model,
-            final HttpServletRequest req, final BindingResult bindingResult) {
-        final Integer tagId = Integer.valueOf(req.getParameter("removeRow"));
-        List<TagProdukModel> listTag = tagProdukDb.findAll();
-        produk.getListTagProduk().remove(tagId.intValue());
-        model.addAttribute("produk", produk);
-        model.addAttribute("listTag", listTag);
-
-        return "produk/tambah-produk";
-    }
-
-    @RequestMapping(value="/produk/tambah", params={"addRow"})
-    public String addRow(
-            @ModelAttribute ProdukModel produk, Model model,
-            final BindingResult bindingResult) {
-        List<TagProdukModel> tagTempList = produk.getListTagProduk();
-        List<TagProdukModel> listTag = tagProdukDb.findAll();
-        TagProdukModel tagGaib = new TagProdukModel();
-
-        produk.setListTagProduk(tagTempList);
-        produk.getListTagProduk().add(tagGaib);
-
-        model.addAttribute("produk", produk);
-        model.addAttribute("listTag", listTag);
-
-        return "produk/tambah-produk";
-    }
-
-
 
     @GetMapping(value = "/produk/tambah")
     public String tambahProduk(Model model) {
@@ -143,37 +108,20 @@ public class ProdukController {
 
     @PostMapping("/produk/tambah")
     public String tambahProdukSubmit(
-            @ModelAttribute ProdukModel produk, Model model){
+            @ModelAttribute ProdukModel produk, Model model, @RequestParam("checkboxName") Integer[] checkboxValue){
 
-        Long harga=produk.getHarga();
-        Integer stok=produk.getStok();
-        if(harga<0){
-            model.addAttribute("pop", "red");
-            model.addAttribute("msg", "Produk Gagal Ditambahkan");
-            model.addAttribute("subMsg", "Harga tidak valid");
-
-        }
-        else if (stok<0){
-            model.addAttribute("pop", "red");
-            model.addAttribute("msg", "Produk Gagal Ditambahkan");
-            model.addAttribute("subMsg", "Stok tidak valid");
-        }
-        else{
-            List<TagProdukModel> list2=produk.getListTagProduk();
-            List<TagProdukModel> list1=new ArrayList<TagProdukModel>();
-            List<ProdukModel> listproduk=new ArrayList<ProdukModel>();
-            produkService.addProduk(produk);
-            for(int i =0; i<list2.size();i++){
-                Long z=Long.parseLong(list2.get(i).getNamaTag());
-                TagProdukModel tag=new TagProdukModel();
-                tag.setIdTagProduk(z);
-                tag.setNamaTag(tagService.getNamaTagById(z));
-                list1.add(tag);
-                listproduk.add(produk);
-                produk.setListTagProduk(list1);
-                tagService.addTagProduk(listproduk);
-            }
-
+        List<TagProdukModel> list1=new ArrayList<TagProdukModel>();
+        List<ProdukModel> listproduk=new ArrayList<ProdukModel>();
+        produkService.addProduk(produk);
+        for(int i =0; i<checkboxValue.length;i++){
+            TagProdukModel tag=new TagProdukModel();
+            Long nilai=Long.valueOf(checkboxValue[i]);
+            tag.setIdTagProduk(nilai);
+            tag.setNamaTag(tagService.getNamaTagById(nilai));
+            list1.add(tag);
+            listproduk.add(produk);
+            produk.setListTagProduk(list1);
+            tagService.addTagProduk(listproduk);
             model.addAttribute("pop", "green");
             model.addAttribute("msg", "Produk Berhasil Ditambahkan");
         }
@@ -185,6 +133,14 @@ public class ProdukController {
     public String ubahstokproduk(@PathVariable Long id, Model model) {
         ProdukModel produk=produkService.getProdukById(id);
         List<TagProdukModel> listTag = tagProdukDb.findAll();
+        List<Long> daftarTag=new ArrayList<>();
+        List<TagProdukModel> listTagProduk=produk.getListTagProduk();
+
+        for(int i=0; i<listTagProduk.size();i++){
+            daftarTag.add(listTagProduk.get(i).getIdTagProduk());
+        }
+        model.addAttribute("daftarTag",daftarTag);
+
 
         model.addAttribute("produk", produk);
         model.addAttribute("listTag", listTag);
@@ -195,63 +151,13 @@ public class ProdukController {
     @PostMapping("/produk/ubah/stok/{id}")
     public String ubahStokProdukSubmit(
             @ModelAttribute ProdukModel produk, @PathVariable Long id, Model model){
-        Integer stok=produk.getStok();
 
-        if(stok<0){
-            model.addAttribute("pop", "red");
-            model.addAttribute("produk", produk);
-            model.addAttribute("msg", "Produk Gagal Diubah");
-            model.addAttribute("subMsg", "Stok tidak valid");
-
-            return "produk/ubah-stok-produk";
-
-        }else{
-            ProdukModel produkUpdated=produkService.updateStokProduk(produk);
-            model.addAttribute("idProduk",produkUpdated.getIdProduk());
-            model.addAttribute("produk", produk);
-            model.addAttribute("pop", "green");
-            model.addAttribute("msg", "Produk Berhasil Diubah");
-            return "produk/ubah-stok-produk";
-        }
-
-    }
-
-    @RequestMapping(value="/produk/ubah/{id}", params={"removeRow"})
-    public String removeRowUbah(@PathVariable Long id,
-                                @ModelAttribute ProdukModel produk, Model model,
-                                final HttpServletRequest req, final BindingResult bindingResult) {
-        final Integer tagId = Integer.valueOf(req.getParameter("removeRow"));
-        List<TagProdukModel> listTag = tagProdukDb.findAll();
-        produk.getListTagProduk().remove(tagId.intValue());
-
-        List<TagProdukModel> listTagProduk=produk.getListTagProduk();
-
-        model.addAttribute("listTags",listTagProduk);
+        ProdukModel produkUpdated=produkService.updateStokProduk(produk);
+        model.addAttribute("idProduk",produkUpdated.getIdProduk());
         model.addAttribute("produk", produk);
-        model.addAttribute("listTag", listTag);
-
-        return "produk/ubah-produk";
-    }
-
-    @RequestMapping(value="/produk/ubah/{id}", params={"addRow"})
-    public String addRowUbah(
-            @ModelAttribute ProdukModel produk, Model model,
-            final BindingResult bindingResult) {
-        List<TagProdukModel> tagTempList = produk.getListTagProduk();
-        List<TagProdukModel> listTag = tagProdukDb.findAll();
-        TagProdukModel tagGaib = new TagProdukModel();
-
-        produk.setListTagProduk(tagTempList);
-        produk.getListTagProduk().add(tagGaib);
-
-        List<TagProdukModel> listTagProduk=produk.getListTagProduk();
-
-        model.addAttribute("listTags",listTagProduk);
-
-        model.addAttribute("produk", produk);
-        model.addAttribute("listTag", listTag);
-
-        return "produk/ubah-produk";
+        model.addAttribute("pop", "green");
+        model.addAttribute("msg", "Produk Berhasil Diubah");
+        return "produk/ubah-stok-produk";
     }
 
     @GetMapping(value = "/produk/ubah/{id}")
@@ -259,10 +165,8 @@ public class ProdukController {
         ProdukModel produk=produkService.getProdukById(id);
         List<TagProdukModel> tagTempList = new ArrayList<TagProdukModel>();
         List<TagProdukModel> listTag = tagProdukDb.findAll();
-
         List<TagProdukModel> listTagProduk=produk.getListTagProduk();
-
-        model.addAttribute("listTags",listTagProduk);
+        List<Long> daftarTag=new ArrayList<>();
 
         if (listTagProduk.size() == 0) {
             TagProdukModel tagGaib = new TagProdukModel();
@@ -270,7 +174,11 @@ public class ProdukController {
             produk.setListTagProduk(tagTempList);
         }
 
-        model.addAttribute("listTags", produk.getListTagProduk());
+        for(int i=0; i<listTagProduk.size();i++){
+            daftarTag.add(listTagProduk.get(i).getIdTagProduk());
+        }
+        model.addAttribute("daftarTag",daftarTag);
+        model.addAttribute("listTags", listTagProduk);
         model.addAttribute("produk", produk);
         model.addAttribute("listTag", listTag);
 
@@ -279,45 +187,28 @@ public class ProdukController {
 
     @PostMapping("/produk/ubah/{id}")
     public String ubahProdukSubmit(
-            @ModelAttribute ProdukModel produk, @PathVariable Long id, Model model) {
-        Long harga = produk.getHarga();
-
-        if (harga < 0) {
-            model.addAttribute("pop", "red");
-            model.addAttribute("produk", produk);
-            model.addAttribute("msg", "Produk Gagal Diubah");
-            model.addAttribute("subMsg", "Harga tidak valid");
-            return "produk/ubah-produk";
+            @ModelAttribute ProdukModel produk, @PathVariable Long id, Model model, @RequestParam("checkboxName") Integer[] checkboxValue) {
+        ProdukModel produk1=produkService.getProdukById(id);
+        tagService.deleteTagProdukUpdate(produk1);
+        List<TagProdukModel> list1=new ArrayList<TagProdukModel>();
+        List<ProdukModel> listproduk=new ArrayList<ProdukModel>();
+        produkService.addProduk(produk);
+        for(int i =0; i< checkboxValue.length;i++) {
+            TagProdukModel tag=new TagProdukModel();
+            Long nilai=Long.valueOf(checkboxValue[i]);
+            tag.setIdTagProduk(nilai);
+            tag.setNamaTag(tagService.getNamaTagById(nilai));
+            list1.add(tag);
+            listproduk.add(produk);
+            produk.setListTagProduk(list1);
+            tagService.updateTagProduk(listproduk,produk1);
         }
-        else {
-            ProdukModel produk1=produkService.getProdukById(id);
-            tagService.deleteTagProdukUpdate(produk1);
-
-            List<TagProdukModel> list2=produk.getListTagProduk();
-            List<TagProdukModel> list1=new ArrayList<TagProdukModel>();
-            List<ProdukModel> listproduk=new ArrayList<ProdukModel>();
-            produkService.addProduk(produk);
-            for(int i =0; i<list2.size();i++) {
-                Long z=Long.parseLong(list2.get(i).getNamaTag());
-                TagProdukModel tag=new TagProdukModel();
-                tag.setIdTagProduk(z);
-                tag.setNamaTag(tagService.getNamaTagById(z));
-                list1.add(tag);
-                listproduk.add(produk);
-                produk.setListTagProduk(list1);
-                tagService.updateTagProduk(listproduk,produk1);
-            }
-
-
-
-            ProdukModel produkUpdated = produkService.updateStokProduk(produk);
-            model.addAttribute("idProduk", produkUpdated.getIdProduk());
-            model.addAttribute("produk", produk);
-            model.addAttribute("pop", "green");
-            model.addAttribute("msg", "Produk Berhasil Diubah");
-            return "produk/ubah-produk";
-        }
-
+        ProdukModel produkUpdated = produkService.updateStokProduk(produk);
+        model.addAttribute("idProduk", produkUpdated.getIdProduk());
+        model.addAttribute("produk", produk);
+        model.addAttribute("pop", "green");
+        model.addAttribute("msg", "Produk Berhasil Diubah");
+        return "produk/ubah-produk";
     }
 
     @ModelAttribute
