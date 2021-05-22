8bbf16c4c10c3b4cfd6c0ad170359b37ead593e2
safafathya <60370165+safafathya@users.noreply.github.com>
2021-05-23 05:56:45 +0700

    UC-11 Mengelola Pameran (#42)
    * feat/uc11-mengelola pameran
    
    * feat/UC11-Mengelola Pameran
    
    * feat/UC11-Mengelola Pameran
    
    * feat: add notification on controller - by Ryan
    
    Co-authored-by: ryananshar <ansharryan@gmail.com>

---
+++ src/main/java/propensi/tugas/pebelco/repository/PameranDb.java
@@ -0,0 +1,12 @@
+package propensi.tugas.pebelco.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import propensi.tugas.pebelco.model.PameranModel;
+
+import java.util.List;
+
+@Repository
+public interface PameranDb extends JpaRepository<PameranModel,Long> {
+    List<PameranModel> findAll();
+}
